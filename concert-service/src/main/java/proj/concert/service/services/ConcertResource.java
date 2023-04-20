3d09aa9833f8dced74a0b4fd1e26cb5b82a58e9f
ParaBalaraman <pbal978@aucklanduni.ac.nz>
2023-04-20 19:25:20 +1200

    Concert Resource Method Husks

--- concert-service/src/main/java/proj/concert/service/services/ConcertResource.java
+++ concert-service/src/main/java/proj/concert/service/services/ConcertResource.java
@@ -1,7 +1,41 @@
 package proj.concert.service.services;
 
-public class ConcertResource {
+import java.net.URI;
+import java.util.Date;
 
-    // TODO Implement this.
+import javax.ws.rs.*;
+
+import javax.ws.rs.core.*;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import proj.concert.service.domain.Concert;
+
+public class ConcertResource {
+    private static final Logger logger = LoggerFactory.getLogger(ConcertResource.class);
+
+    public Response getSingleConcert(long id){return null;}
+
+    public Response getAllConcerts(){return null;}
+
+    public Response getConcertSummaries(){return null;}
+
+    public Response getSinglePerformer(long id){return null;}
+
+    public Response getAllPerformers(){return null;}
+
+    public Response login(String username, String password){return null;}
+
+    public Response bookSeats(long id, Date date, String seats, Cookie userId){return null;}
+
+    public Response getBookingById(long id){return null;}
+
+    public Response getAllBookings(){return null;}
+
+    public Response getSeatsForDate(Date date, String status){return null;}
 
 }

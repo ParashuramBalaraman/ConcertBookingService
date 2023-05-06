ec3101897d1c8f840923c0d0f4966ce7a5b98218
Matt <your>
2023-05-07 02:25:59 +1200

    made annotations concise,removed redundant mappers

--- concert-service/src/main/java/proj/concert/service/domain/mapper/PerformerMapper.java
+++ concert-service/src/main/java/proj/concert/service/domain/mapper/PerformerMapper.java
@@ -4,13 +4,7 @@ import proj.concert.common.dto.PerformerDTO;
 import proj.concert.service.domain.Performer;
 
 public class PerformerMapper {
-    public static Performer toDM(PerformerDTO performerDTO){
-        return new Performer(performerDTO.getId(),
-                performerDTO.getName(),
-                performerDTO.getImageName(),
-                performerDTO.getGenre(),
-                performerDTO.getBlurb());
-    }
+
     public static PerformerDTO toDTO(Performer performer){
         return new PerformerDTO(performer.getId(),
                 performer.getName(),
